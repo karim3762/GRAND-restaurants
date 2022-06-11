bb2de6129d44249f82dbaafe06307566fa3d453d
Mohamedamr9 <106562457+Mohamedamr9@users.noreply.github.com>
2022-06-12 00:47:49 +0200

    Add files via upload

--- about.php
+++ about.php
@@ -1,8 +1,16 @@
-<!DOCTYPE html>
+<?php 
+session_start();
+if($_SESSION["Role"]!=1){
+	header("location:outofscope.php");
+}
+include "navtest.php"; 
+?>
+
 <html>
 <head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>About</title>
-	
 </head>
 <style>
 	body{
@@ -11,7 +19,7 @@
  padding: 0px;
  box-sizing: border-box;
 font-family: 'Times New Roman', serif;
- background-image:url('home2.jpg');  
+ background-image:url('homee.jpg');  
   background-repeat: ;
  background-size: cover;
 }
@@ -19,6 +27,8 @@ font-family: 'Times New Roman', serif;
   border-style: 1px solid;
   width: 85%;height: auto;
   margin: auto;
+
+
 
 }
 .h
@@ -35,50 +45,41 @@ padding-top: 10px;
   color: white;
   font-size: 25px;
   margin: auto;
-  text-align: center;
-
+  text-align: left;
 }
 .he{
   padding-left: 8px;
   margin: auto; 
   text-transform: uppercase;
-  color: white;
+  color: antiquewhite;
    text-align: center; 
   text-decoration: underline;
-font-size: 25px;
+padding-top: 10px; 
 
 }
-.contain
+.overview
 {
-  width: 60%;
-  border: 20px;
-  border-radius:15px ;
-  background-color: rgba(0,0,0,0.8);
-  margin: auto;
-  text-align: center;
-  padding: 15px;  
-}
-.imgcontainer {
-  text-align: center;
-  margin:auto;
+
 }
 </style>
 <body>
-  <?php include"nav.php";?>
-    <div class="imgcontainer">
-  <img src="lastlogo4.png" alt="img" >
-  </div><div class="contain">
-		<h2 class="he">Cairo GRND Restaurant is one of the biggest restaurants in Cairo. <br>It has been operating in Cairo
-since 1910. </h2><br>
- 
-  <p class="he" style="font-size:22px;">     
+  <h1 class="h">ABOUT</h1>
+<div class="contain-word">
+	<div class="overview">
+		<p class="he">Cairo GRND Restaurant is one of the biggest restaurants in Cairo. It has been operating in Cairo
+since 1910. </p>
+	</div>
+  <br/><br/>
+  <p class="he" style="font-size:27px;">     
   -Exclusive reservation for the Marquise boat is available for groups and weddings.</p>
                                     <h3 class="he"><strong>Opening Hours</strong></h3><br>
   <p class="words">
-  - Breakfast cruise: everyday from 08:00 pm to 10:00 pm <br>                            
+  - Breakfast cruise: everyday from 08:00 pm to 10:00 pm  <br>
+  - Launch cruise: everyday from 08:00 pm to 10:00 pm <br>                            
   - Dinner cruise: everyday from 03:00 pm to 05:00 pm<br>
   For reservations, please call: 0223651234</p>
-</div>
 
+
+</div>
 </body>
 </html>
